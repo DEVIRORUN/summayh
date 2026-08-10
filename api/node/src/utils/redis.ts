4f5eff998cc3c9719ec2339125f89bcca0ab73a5
DEVIRORUN <irorun757@gmail.com>
2026-08-10 13:47:28 +0000

    Added retry logic

--- api/node/src/utils/redis.ts
+++ api/node/src/utils/redis.ts
@@ -3,6 +3,14 @@ import Redis from "ioredis";
 export function createBullMQConnection() {
     const redis = new Redis(process.env.REDIS_URL!, {
         maxRetriesPerRequest: null,
+        connectTimeout: 10000,
+        retryStrategy(times) {
+            if (times > 5) {
+                console.error("Redis: giving up after 5 retries");
+                return null;
+            }
+            return Math.min(times * 200, 2000)
+        }
     });
     
     redis.on("connect", () => console.log("Redis connected"))
